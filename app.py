b34cf16e9b903e82cfc2a5b0406984cfcff52ced
Akash Karanam <Karanam.akash1002@rice.edu>
2022-09-08 17:57:16 -0500

    Resolves #3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request, session
 from forms import SearchForm
 
 app = Flask(__name__)
@@ -9,13 +9,12 @@ app.config['SECRET_KEY'] = '7a3140fad78d44bd'
 def home():
     form = SearchForm()
     if form.validate_on_submit():
-      flash(f'Search query is {form.query}', 'success')
-      return redirect(url_for('search'))
+      return redirect(url_for('search', query=request.form['query']))
     return render_template('home.html', form=form)
 
-@app.route("/search")
-def search():
-  return render_template('search.html')
+@app.route("/search/<query>")
+def search(query):
+  return render_template('search.html', query=query)
 
   
 if __name__ == '__main__':
