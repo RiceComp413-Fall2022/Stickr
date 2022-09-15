76d992f771bcbb1f3157e43ca989db1e8f9c153e
Ayush Sachdeva <as216@rice.edu>
2022-09-14 21:10:52 -0500

    Changing structure to poetry

---
+++ stickr/stickr/sticker_generation.py
@@ -0,0 +1,11 @@
+import requests
+
+def generate_sticker(query):
+  r = requests.post(
+    'https://api.deepai.org/api/text2img',
+    data={
+      'text': query,
+    },
+    headers={'api-key': '8bc69e15-14aa-49ce-9b62-7a28f2efa916'}
+  )
+  return r.json()['output_url']
