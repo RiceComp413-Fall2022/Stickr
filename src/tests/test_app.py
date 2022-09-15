c4d36aba94d2e3b40aee19450d89f7c050238eb5
Akash Karanam <Karanam.akash1002@rice.edu>
2022-09-14 21:49:09 -0500

    added tests for search on tab string or space string

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,6 +4,20 @@ import stickr.app as app
 def test_search_empty():
   try:
     app.search("")
-    pytest.fail("Empty search did not throw exception")
+    pytest.fail("Searching for empty string did not throw exception.")
+  except Exception as ex:
+    pass
+
+def test_search_tab():
+  try:
+    app.search("  ")
+    pytest.fail("Searching for tab string did not throw exception.")
+  except Exception as ex:
+    pass
+
+def test_search_space():
+  try:
+    app.search(" ")
+    pytest.fail("Searching for space string did not throw exception.")
   except Exception as ex:
     pass
