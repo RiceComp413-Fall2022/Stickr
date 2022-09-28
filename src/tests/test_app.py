507b5705e1d8d48277964fad1482de5246c87285
Akash Karanam <Karanam.akash1002@rice.edu>
2022-09-28 15:23:45 -0500

    Rename app as application in the test code

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,23 +1,23 @@
 import pytest
-import stickr.app as app
+import stickr.application as application
 
 def test_search_empty():
   try:
-    app.search("")
+    application.search("")
     pytest.fail("Searching for empty string did not throw exception.")
   except Exception as ex:
     pass
 
 def test_search_tab():
   try:
-    app.search("  ")
+    application.search("  ")
     pytest.fail("Searching for tab string did not throw exception.")
   except Exception as ex:
     pass
 
 def test_search_space():
   try:
-    app.search(" ")
+    application.search(" ")
     pytest.fail("Searching for space string did not throw exception.")
   except Exception as ex:
     pass
