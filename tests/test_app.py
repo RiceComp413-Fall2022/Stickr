ba6dc31ff1959e3dfc2a906d237bd44206c0cf8c
Akash Karanam <Karanam.akash1002@rice.edu>
2022-09-29 15:20:28 -0500

    Reorganized project structure and was able to deploy to aws

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,23 @@
 import pytest
-import stickr.application as application
+import application as app
 
 def test_search_empty():
   try:
-    application.search("")
+    app.search("")
     pytest.fail("Searching for empty string did not throw exception.")
   except Exception as ex:
     pass
 
 def test_search_tab():
   try:
-    application.search("  ")
+    app.search("  ")
     pytest.fail("Searching for tab string did not throw exception.")
   except Exception as ex:
     pass
 
 def test_search_space():
   try:
-    application.search(" ")
+    app.search(" ")
     pytest.fail("Searching for space string did not throw exception.")
   except Exception as ex:
     pass
