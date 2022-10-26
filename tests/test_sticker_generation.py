4e54264f9a78886f4ecb4407a9f252aaa55c886a
bthomas4 <66330510+bthomas4@users.noreply.github.com>
2022-10-25 21:42:06 -0500

    Refactored prompt engineering, changed form to accept cutout specification (need to change tests)

--- tests/test_sticker_generation.py
+++ tests/test_sticker_generation.py
@@ -1,36 +1,39 @@
 import pytest
-from src.sticker_generation import prepend_sticker_design_of
+from src.sticker_generation import engineer_prompt
 
-prepend_string = "sticker design of "
+prepend_string = "sticker illustration of "
+append_string = ""
+append_string1 = ", circle cutout"
+append_string2 = ", square cutout"
 query_str = "cats in space"
 
 def test_parse_empty():
-    result = prepend_sticker_design_of("")
-    if result != prepend_string:
-        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
+    result = engineer_prompt("")
+    if result != "":
+        pytest.fail(f"Yielded: '{result}''; Expected: ''.")
 
 def test_parse_tab():
-    result = prepend_sticker_design_of("    ")
+    result = engineer_prompt("    ")
     if result != prepend_string:
         pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
 
 def test_parse_space():
-    result = prepend_sticker_design_of(" ")
+    result = engineer_prompt(" ")
     if result != prepend_string:
         pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
 
 def test_parse_no_prepend_already():
-    result = prepend_sticker_design_of(query_str)
+    result = engineer_prompt(query_str)
     if result != prepend_string+query_str:
         pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
 
 def test_parse_has_prepend_already():
-    result = prepend_sticker_design_of(f"{prepend_string}{query_str}")
+    result = engineer_prompt(f"{prepend_string}{query_str}")
     print(result)
     if result != prepend_string+query_str:
         pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}' .")
 
 def test_parse_has_prepend_with_surrounding_space():
-    result = prepend_sticker_design_of(f" {prepend_string}{query_str}     ")
+    result = engineer_prompt(f" {prepend_string}{query_str}     ")
     if result != prepend_string+query_str:
         pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
