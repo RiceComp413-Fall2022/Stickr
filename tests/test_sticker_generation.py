dd04be52478131b8ac766392c2c6d608cd148326
bthomas4 <66330510+bthomas4@users.noreply.github.com>
2022-10-26 15:38:06 -0500

    fixed test cases for cutouts

--- tests/test_sticker_generation.py
+++ tests/test_sticker_generation.py
@@ -1,39 +1,41 @@
 import pytest
 from src.sticker_generation import engineer_prompt
 
-prepend_string = "sticker illustration of "
+prepend_string = "Sticker illustration of "
 append_string = ""
 append_string1 = ", circle cutout"
 append_string2 = ", square cutout"
 query_str = "cats in space"
 
+default_cutout = 'default'
+
 def test_parse_empty():
-    result = engineer_prompt("")
+    result = engineer_prompt("", default_cutout)
     if result != "":
-        pytest.fail(f"Yielded: '{result}''; Expected: ''.")
+        pytest.fail(f"Yielded: '{result}'; Expected empty string")
 
 def test_parse_tab():
-    result = engineer_prompt("    ")
-    if result != prepend_string:
-        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
+    result = engineer_prompt("    ", default_cutout)
+    if result != "":
+        pytest.fail(f"Yielded: '{result}''; Expected empty string.")
 
 def test_parse_space():
-    result = engineer_prompt(" ")
-    if result != prepend_string:
-        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
+    result = engineer_prompt(" ", default_cutout)
+    if result != "":
+        pytest.fail(f"Yielded: '{result}''; Expected: empty string.")
 
 def test_parse_no_prepend_already():
-    result = engineer_prompt(query_str)
+    result = engineer_prompt(query_str, default_cutout)
     if result != prepend_string+query_str:
-        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
+        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}{query_str}'.")
 
 def test_parse_has_prepend_already():
-    result = engineer_prompt(f"{prepend_string}{query_str}")
+    result = engineer_prompt(f"{prepend_string}{query_str}", default_cutout)
     print(result)
     if result != prepend_string+query_str:
-        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}' .")
+        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}{query_str}' .")
 
 def test_parse_has_prepend_with_surrounding_space():
-    result = engineer_prompt(f" {prepend_string}{query_str}     ")
+    result = engineer_prompt(f" {prepend_string}{query_str}     ", default_cutout)
     if result != prepend_string+query_str:
-        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}'.")
+        pytest.fail(f"Yielded: '{result}''; Expected: '{prepend_string}{query_str}'.")
