29e3cb1811c61425ebf26fa79ce73a10e4586b96
Ayush Sachdeva <as216@rice.edu>
2022-10-31 17:59:30 -0500

    Added crop functionality to image 1 (WIP)

--- static/search.js
+++ static/search.js
@@ -31,6 +31,9 @@ let firstImageModalBtn = document.getElementById("openFirstImageModal");
 let secondImageModalBtn = document.getElementById("openSecondImageModal");
 let thirdImageModalBtn = document.getElementById("openThirdImageModal");
 let fourthImageModalBtn = document.getElementById("openFourthImageModal");
+
+let firstImageCropBtn = document.getElementById("cropFirstImage");
+
 let currFirstImage = document.getElementById("firstImage");
 let currSecondImage = document.getElementById("secondImage");
 let currThirdImage = document.getElementById("thirdImage");
@@ -180,6 +183,182 @@ function renderFourthImage() {
   fourthImage.setAttribute('crossorigin', 'anonymous');
 }
 
+function draw(c, node1, node2, node3, node4, boundary) {
+  //draw in the container
+  // c.fillStyle = "#000000";
+  // c.fillRect(container.y, container.x, container.width, container.height);
+  // renderFirstImage();
+
+  //draw first node
+  c.arc(node1.x, node1.y, node1.r, 0, 2 * Math.PI);
+  c.fillStyle = node1.color;
+  c.fill()
+  c.closePath();
+  
+  //draw second node
+  c.arc(node2.x, node2.y, node2.r, 0, 2 * Math.PI);
+  c.strokeStyle = node2.color;
+  c.fillStyle = node2.color;
+  c.fill()
+  c.closePath();
+
+  //draw third node
+  c.arc(node3.x, node3.y, node3.r, 0, 2 * Math.PI);
+  c.strokeStyle = node3.color;
+  c.fillStyle = node3.color;
+  c.fill()
+  c.closePath();
+
+  //draw fourth node
+  c.arc(node4.x, node4.y, node4.r, 0, 2 * Math.PI);
+  c.strokeStyle = node4.color;
+  c.fillStyle = node4.color;
+  c.fill()
+  c.closePath();
+
+  //draw boundary
+  c.beginPath();
+  c.moveTo(boundary.node1x, boundary.node1y);
+  c.lineTo(boundary.node2x, boundary.node2y);
+
+  c.moveTo(boundary.node2x, boundary.node2y);
+  c.lineTo(boundary.node3x, boundary.node3y);
+
+  c.moveTo(boundary.node3x, boundary.node3y);
+  c.lineTo(boundary.node4x, boundary.node4y);
+
+  c.moveTo(boundary.node4x, boundary.node4y);
+  c.lineTo(boundary.node1x, boundary.node1y);
+  
+  c.strokeStyle = boundary.color;
+  c.lineWidth = boundary.width;
+  c.closePath();
+  c.stroke();
+}
+
+function Node(x, y, r, color) {
+  this.x = x;
+  this.y = y;
+  this.r = r || 15;
+  this.color = color || "#ff0";
+}
+
+function Box(node1, node2, node3, node4, width, color) {
+  this.node1 = node1;
+  this.node2 = node2;
+  this.node3 = node3;
+  this.node4 = node4;
+  this.width = width || 5;
+  this.color = color || "#f00";
+  Object.defineProperties(this, {
+    node1x: {
+      "get": () => this.node1.x,
+      "set": x => { this.node1.x = x }
+    },
+    node1y: {
+      "get": () => this.node1.y,
+      "set": y => { this.node1.y = y }
+    },
+    node2x: {
+      "get": () => this.node2.x,
+      "set": x => { this.node2.x = x }
+    },
+    node2y: {
+      "get": () => this.node2.y,
+      "set": y => { this.node2.y = y }
+    },
+    node3x: {
+      "get": () => this.node3.x,
+      "set": x => { this.node3.x = x }
+    },
+    node3y: {
+      "get": () => this.node3.y,
+      "set": y => { this.node3.y = y }
+    },
+    node4x: {
+      "get": () => this.node4.x,
+      "set": x => { this.node4.x = x }
+    },
+    node4y: {
+      "get": () => this.node4.y,
+      "set": y => { this.node4.y = y }
+    }
+  })
+}
+
+function handleMouseDrag(canvas, nodes) {
+  var isDrag = false;
+  var offset = { x: 0, y: 0, x0: 0, y0: 0 };
+  var dragNode = undefined;
+  canvas.addEventListener("mousedown", function (e) {
+    var x = e.offsetX, y = e.offsetY;
+    for (var i in nodes) {
+      if (Math.pow(x - nodes[i].x, 2) + Math.pow(y - nodes[i].y, 2) < Math.pow(nodes[i].r, 2)) {
+        isDrag = true;
+        dragNode = nodes[i];
+        offset = { x: dragNode.x, y: dragNode.y, x0: x, y0: y };
+        return;
+      }
+      else if (i == 1){
+        // window.alert(x);
+        // window.alert(nodes[i].x);
+        // window.alert(y);
+        // window.alert(nodes[i].y);
+        
+      }
+    }
+  });
+  canvas.addEventListener("mousemove", function (e) {
+    if (isDrag) {
+      dragNode.x = e.offsetX - offset.x0 + offset.x;
+      dragNode.y = e.offsetY - offset.y0 + offset.y;
+    }
+  });
+  canvas.addEventListener("mouseup", function (e) {
+    isDrag = false;
+  });
+  canvas.addEventListener("mouseleave", function (e) {
+    isDrag = false;
+  });
+}
+
+function renderFirstCropSquare() {
+
+  node_radius = 15
+  var node1 = new Node(0, 0, node_radius);
+  // var node2 = new Node(0, 0);
+  // var node3 = new Node(0, 0);
+  // var node4 = new Node(0, 0);
+
+  var node2 = new Node(firstImageCanvas.width, 0, node_radius);
+  var node3 = new Node(firstImageCanvas.width, firstImageCanvas.height, node_radius);
+  var node4 = new Node(0, firstImageCanvas.height, node_radius);
+
+  var boundingBox = new Box(node1, node2, node3, node4);
+  
+  handleMouseDrag(firstImageCanvas, [node1, node2, node3, node4]);
+
+  function updateFrame() {
+    firstCanvasCtx.save();
+    renderFirstImage();
+    draw(firstCanvasCtx, node1, node2, node3, node4, boundingBox);
+    firstCanvasCtx.restore();
+    requestAnimationFrame(updateFrame)
+  };
+  updateFrame();
+
+  // firstCanvasCtx.beginPath();
+  // firstCanvasCtx.lineWidth = "6";
+  // firstCanvasCtx.strokeStyle = "red";
+  // firstCanvasCtx.rect(0, 0, firstImageCanvas.width, firstImageCanvas.height);
+  // firstCanvasCtx.stroke();
+
+  // // firstCanvasCtx.drawImage(image, 150, 200, 500, 300, 60,60, 500, 300);
+  // firstCanvasCtx.drawImage(firstImage, 0, 0, 256, 256, 0, 0, 512, 512)
+  // firstCanvasCtx.drawImage(firstImage, 150, 200, 500, 300, 60,60, 500, 300);
+
+}
+
 function saveFirstImage() {
   let newUrl = firstImageCanvas.toDataURL();
   currFirstImage.setAttribute('crossorigin', 'anonymous');
@@ -243,6 +422,11 @@ fourthImageModalBtn.addEventListener('click', event => {
   renderFourthImage();
   resetFourthImageSettings();
 });
+
+firstImageCropBtn.addEventListener('click', event => {
+  renderFirstCropSquare();
+});
+
 
 firstImageBrightnessInput.addEventListener("change", () =>
   updateFirstImageSetting("brightness", firstImageBrightnessInput.value)
